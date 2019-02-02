90a52d8a645ecce992585983b263affb0c22435c
Abhilash Engala <Elizabeth@deep-mac-mini.local>
2019-02-02 18:44:11 +0530

    Initial Commit

---
+++ BasicProjectClassesFW/BasicProjectClassesFW.h
@@ -0,0 +1,19 @@
+//
+//  BasicProjectClassesFW.h
+//  BasicProjectClassesFW
+//
+//  Created by Elizabeth on 02/02/19.
+//  Copyright © 2019 tvisha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicProjectClassesFW.
+FOUNDATION_EXPORT double BasicProjectClassesFWVersionNumber;
+
+//! Project version string for BasicProjectClassesFW.
+FOUNDATION_EXPORT const unsigned char BasicProjectClassesFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicProjectClassesFW/PublicHeader.h>
+
+
